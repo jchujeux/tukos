4468904a1d9f63bed40022cf86542a67d5b15663
Jean-Claude Hujeux <jchujeux@gmail.com>
2024-12-10 16:16:49 +0100

    Security fix

--- site/testGeminiAPI.php
+++ site/testGeminiAPI.php
@@ -27,28 +27,13 @@ try {
      ->withBaseUrl('https://generativelanguage.googleapis.com/v1/') // default: https://generativelanguage.googleapis.com/v1/
      ->withHttpHeader('X-My-Header', 'foo')
      ->withQueryParam('my-param', 'bar')
-     ->withHttpClient(new \GuzzleHttp\Client([]))  // default: HTTP client found using PSR-18 HTTP Client Discovery
-     /*->withStreamHandler(fn(RequestInterface $request): ResponseInterface => $client->send($request, [
-       'stream' => true // Allows to provide a custom stream handler for the http client.
-     ]))*/
+     ->withHttpClient(new \GuzzleHttp\Client([])) 
      ->make();
      $result = $client->geminiPro()->generateContent(mb_convert_encoding("Ecris-moi une introduction pour un rapport d'inspection électrique pour un spectacle.", 'UTF-8'));
      $textResult = $result->text();
      print iconv('UTF-8', 'ISO-8859-1', $textResult);
      var_dump(Utl::utf8($result->text())); // Hello! How can I assist you today?
      
- /*    $factory = new HttpFactory;
-    $http = $factory->newInstance('curl');
-    $request = $http->newRequest();
-    $request->headers->set("Content-Type",  'application/json');
-    $request->setUrl("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDjiCA2MGTFj1wmKaylLg7AsaSAgDGBTDY");
-    $request->setContent('[{"parts":[{"text": "Explain how AI works"}]');
-    $request->setMethod('POST');
-    
-    $response = $http->send($request);
-
-    echo var_dump($response);
-*/
 } catch(Exception $e) {
     print $e->getMessage();
 }
